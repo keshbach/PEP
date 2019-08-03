45ac73c4a88b5b1a91f9777dd3ce2ac18384608d
Kevin Eshbach <keshbach@comcast.net>
2019-08-03 13:28:09 -0400

    Preliminary work to allow logging to a file.

--- Source/Drivers/PepCtrl/PepCtrlLog.c
+++ Source/Drivers/PepCtrl/PepCtrlLog.c
@@ -10,8 +10,58 @@
 
 #if !defined(NDEBUG)
 
+#if defined(ALLOC_PRAGMA)
+#pragma alloc_text (PAGE, PepCtrlLogOpenFile)
+#pragma alloc_text (PAGE, PepCtrlLogCloseFile)
+#endif
+
+static HANDLE l_hFile = NULL;
+
+VOID PepCtrlLogOpenFile(
+  _In_z_ PCWSTR pszLogFile)
+{
+    NTSTATUS Status;
+    OBJECT_ATTRIBUTES ObjectAttributes;
+    UNICODE_STRING FileName;
+    IO_STATUS_BLOCK StatusBlock;
+
+    PAGED_CODE()
+
+    if (KeGetCurrentIrql() != PASSIVE_LEVEL)
+    {
+        return;
+    }
+
+    RtlInitUnicodeString(&FileName, pszLogFile);
+
+    InitializeObjectAttributes(&ObjectAttributes, &FileName,
+                               OBJ_KERNEL_HANDLE | OBJ_CASE_INSENSITIVE,
+                               NULL, NULL);
+
+    Status = ZwCreateFile(&l_hFile, GENERIC_WRITE, &ObjectAttributes,
+                          &StatusBlock, NULL, FILE_ATTRIBUTE_NORMAL | FILE_SHARE_READ,
+                          0, FILE_OVERWRITE_IF, FILE_SYNCHRONOUS_IO_NONALERT,
+                          NULL, 0);
+}
+
+VOID PepCtrlLogCloseFile()
+{
+    PAGED_CODE()
+
+    if (l_hFile)
+    {
+        ZwClose(l_hFile);
+
+        l_hFile = NULL;
+    }
+}
+
 VOID __cdecl PepCtrlLog(_In_z_ _Printf_format_string_ PCSTR pszFormat, ...)
 {
+    size_t nDataLen;
+    NTSTATUS Status;
+    IO_STATUS_BLOCK StatusBlock;
+
     va_list arguments;
 
     va_start(arguments, pszFormat);
@@ -19,6 +69,14 @@ VOID __cdecl PepCtrlLog(_In_z_ _Printf_format_string_ PCSTR pszFormat, ...)
     vDbgPrintExWithPrefix("PepCtrl: ", DPFLTR_IHVDRIVER_ID, DPFLTR_ERROR_LEVEL, pszFormat, arguments);
 
     va_end(arguments);
+
+    if (l_hFile && KeGetCurrentIrql() == PASSIVE_LEVEL)
+    {
+        nDataLen = RtlStringCbLengthA(pszFormat, NTSTRSAFE_MAX_LENGTH, &nDataLen);
+
+        Status = ZwWriteFile(l_hFile, NULL, NULL, NULL, &StatusBlock,
+                             (PVOID)pszFormat, (ULONG)nDataLen, NULL, NULL);
+    }
 }
 
 #endif
--- Source/Drivers/PepCtrl/PepCtrlLog.h
+++ Source/Drivers/PepCtrl/PepCtrlLog.h
@@ -6,8 +6,14 @@
 #define PepCtrlLog_H
 
 #if !defined(NDEBUG)
+VOID PepCtrlLogOpenFile(_In_z_ PCWSTR pszLogFile);
+VOID PepCtrlLogCloseFile();
+
 VOID __cdecl PepCtrlLog(_In_z_ _Printf_format_string_ PCSTR pszFormat, ...);
 #else
+#define PepCtrlLogOpenFile(_x_)
+#define PepCtrlLogCloseFile()
+
 #define PepCtrlLog(_x_) 
 #endif
 
