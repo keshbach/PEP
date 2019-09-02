f233c0fcd0e4e78408f0cbece9a9cbdf01324e74
Kevin Eshbach <keshbach@comcast.net>
2019-09-01 20:39:12 -0400

    Added SAL annotations and made paged memory functions pageable.

--- Source/Utils/UtHeapDriver.h
+++ Source/Utils/UtHeapDriver.h
@@ -7,12 +7,19 @@
 
 #define UTHEAPDRIVERAPI __stdcall
 
+_IRQL_requires_max_(PASSIVE_LEVEL)
 VOID UTHEAPDRIVERAPI UtInitMemPoolTag(_In_ ULONG Tag);
 
+_IRQL_requires_max_(APC_LEVEL)
 PVOID UTHEAPDRIVERAPI UtAllocPagedMem(_In_ SIZE_T NumberOfBytes);
+
+_IRQL_requires_max_(DISPATCH_LEVEL)
 PVOID UTHEAPDRIVERAPI UtAllocNonPagedMem(_In_ SIZE_T NumberOfBytes);
 
+_IRQL_requires_max_(APC_LEVEL)
 VOID UTHEAPDRIVERAPI UtFreePagedMem(_In_ PVOID pvMem);
+
+_IRQL_requires_max_(DISPATCH_LEVEL)
 VOID UTHEAPDRIVERAPI UtFreeNonPagedMem(_In_ PVOID pvMem);
 
 #endif /* !defined(UtHeapDriver_H) */
--- Source/Utils/UtHeapDriver/UtHeapDriver.c
+++ Source/Utils/UtHeapDriver/UtHeapDriver.c
@@ -8,53 +8,68 @@
 
 #ifdef ALLOC_PRAGMA
 #pragma alloc_text (PAGE, UtInitMemPoolTag)
+#pragma alloc_text (PAGE, UtAllocPagedMem)
+#pragma alloc_text (PAGE, UtFreePagedMem)
 #endif
 
-static ULONG l_MemPoolTag = 0;
+#pragma region "Local Variables"
 
+static ULONG l_MemPoolTag = 0;
+static POOL_TYPE l_NonPagedPoolType = NonPagedPool;
+
+#pragma endregion
+
+_IRQL_requires_max_(PASSIVE_LEVEL)
 VOID UTHEAPDRIVERAPI UtInitMemPoolTag(
   _In_ ULONG Tag)
 {
+    RTL_OSVERSIONINFOW VersionInfo;
+
     PAGED_CODE()
 
     l_MemPoolTag = Tag;
-}
-
-PVOID UTHEAPDRIVERAPI UtAllocPagedMem(
-  _In_ SIZE_T NumberOfBytes)
-{
-    return ExAllocatePoolWithTag(PagedPool, NumberOfBytes, l_MemPoolTag);
-}
-
-PVOID UTHEAPDRIVERAPI UtAllocNonPagedMem(
-  _In_ SIZE_T NumberOfBytes)
-{
-    POOL_TYPE PoolType = NonPagedPool;
-    RTL_OSVERSIONINFOW VersionInfo;
 
     VersionInfo.dwOSVersionInfoSize = sizeof(VersionInfo);
-    
+
     if (STATUS_SUCCESS == RtlGetVersion(&VersionInfo))
     {
         if (VersionInfo.dwMajorVersion > 6)
         {
-            PoolType = NonPagedPoolNx;
+            l_NonPagedPoolType = NonPagedPoolNx;
         }
         else if (VersionInfo.dwMajorVersion == 6 && VersionInfo.dwMinorVersion >= 2)
         {
-            PoolType = NonPagedPoolNx;
+            l_NonPagedPoolType = NonPagedPoolNx;
         }
     }
-
-    return ExAllocatePoolWithTag(PoolType, NumberOfBytes, l_MemPoolTag);
 }
 
+_IRQL_requires_max_(APC_LEVEL)
+PVOID UTHEAPDRIVERAPI UtAllocPagedMem(
+  _In_ SIZE_T NumberOfBytes)
+{
+    PAGED_CODE()
+        
+    return ExAllocatePoolWithTag(PagedPool, NumberOfBytes, l_MemPoolTag);
+}
+
+_IRQL_requires_max_(DISPATCH_LEVEL)
+PVOID UTHEAPDRIVERAPI UtAllocNonPagedMem(
+  _In_ SIZE_T NumberOfBytes)
+{
+    return ExAllocatePoolWithTag(l_NonPagedPoolType, NumberOfBytes, l_MemPoolTag);
+}
+
+_IRQL_requires_max_(APC_LEVEL)
 VOID UTHEAPDRIVERAPI UtFreePagedMem(
   _In_ PVOID pvMem)
 {
+    PAGED_CODE()
+
     ExFreePoolWithTag(pvMem, l_MemPoolTag);
 }
 
+_IRQL_requires_max_(DISPATCH_LEVEL)
 VOID UTHEAPDRIVERAPI UtFreeNonPagedMem(
   _In_ PVOID pvMem)
 {
