012490e316e34a6ec7bfbacda0220e326482487a
Kevin Eshbach <keshbach@comcast.net>
2020-03-16 21:10:43 -0400

    Tweaked the name given to a new checked list box item in the collection editor.

--- Source/UtilsPepNet/UiPepCtrlsNet/CheckedListBoxItemCollectionEditor.cpp
+++ Source/UtilsPepNet/UiPepCtrlsNet/CheckedListBoxItemCollectionEditor.cpp
@@ -15,14 +15,37 @@
 
 #include "CheckedListBoxItemCollectionEditor.h"
 
+static array<System::Object^>^ lGetCheckedListBoxItemArray(
+  System::Windows::Forms::Form^ Form)
+{
+	System::Reflection::PropertyInfo^ PropInfo = nullptr;
+	System::Object^ Object;
+
+	try
+	{
+		PropInfo = Form->GetType()->GetProperty("Items", System::Reflection::BindingFlags::Instance | System::Reflection::BindingFlags::NonPublic);
+	}
+	catch (System::Exception^)
+	{
+	}
+
+	if (PropInfo != nullptr)
+	{
+		Object = PropInfo->GetValue(Form, nullptr);
+
+		return (array<System::Object^>^)Object;
+	}
+
+	return nullptr;
+}
+
 Pep::Forms::CheckedListBoxItemCollectionEditor::CheckedListBoxItemCollectionEditor() :
 	CollectionEditor(CheckedListBoxItem::typeid)
 {
 }
 
 Pep::Forms::CheckedListBoxItemCollectionEditor::CheckedListBoxItemCollectionEditor(
-  System::Type^ type) : 
-	CollectionEditor(type)
+   System::Type^ type) : CollectionEditor(type)
 {
 }
 
@@ -34,11 +57,54 @@ System::ComponentModel::Design::CollectionEditor::CollectionForm^ Pep::Forms::Ch
 {
 	System::ComponentModel::Design::CollectionEditor::CollectionForm^ Form = System::ComponentModel::Design::CollectionEditor::CreateCollectionForm();
 	System::Type^ Type = Form->GetType();
-	System::Reflection::PropertyInfo^ PropInfo = Type->GetProperty("CollectionEditable", System::Reflection::BindingFlags::Instance | System::Reflection::BindingFlags::NonPublic);
+	System::Reflection::PropertyInfo^ PropInfo = nullptr;
 
-	PropInfo->SetValue(Form, true, nullptr);
+	try
+	{
+		PropInfo = Type->GetProperty("CollectionEditable", System::Reflection::BindingFlags::Instance | System::Reflection::BindingFlags::NonPublic);
+	}
+	catch (System::Exception^)
+	{
+	}
+
+	if (PropInfo != nullptr)
+	{
+		PropInfo->SetValue(Form, true, nullptr);
+	}
+
+	m_CollectionForm = Form;
+
+	m_CollectionForm->FormClosed += gcnew System::Windows::Forms::FormClosedEventHandler(this, &Pep::Forms::CheckedListBoxItemCollectionEditor::CollectionForm_FormClosed);
 
 	return Form;
+}
+
+System::Object^ Pep::Forms::CheckedListBoxItemCollectionEditor::CreateInstance(
+  System::Type^ ItemType)
+{
+	if (ItemType == CheckedListBoxItem::typeid)
+	{
+		CheckedListBoxItem^ Item = gcnew CheckedListBoxItem();
+		array<System::Object^>^ CheckedListBoxItemArray = lGetCheckedListBoxItemArray(m_CollectionForm);
+
+		if (CheckedListBoxItemArray != nullptr)
+		{
+			Item->Name = System::String::Format("Item #{0}", CheckedListBoxItemArray->Length + 1);
+
+			return Item;
+		}
+	}
+
+	return System::ComponentModel::Design::CollectionEditor::CreateInstance(ItemType);
+}
+
+void Pep::Forms::CheckedListBoxItemCollectionEditor::CollectionForm_FormClosed(
+  System::Object^ sender,
+  System::Windows::Forms::FormClosedEventArgs^ e)
+{
+	m_CollectionForm->FormClosed -= gcnew System::Windows::Forms::FormClosedEventHandler(this, &Pep::Forms::CheckedListBoxItemCollectionEditor::CollectionForm_FormClosed);
+
+	m_CollectionForm = nullptr;
 }
 
 /////////////////////////////////////////////////////////////////////////////
--- Source/UtilsPepNet/UiPepCtrlsNet/CheckedListBoxItemCollectionEditor.h
+++ Source/UtilsPepNet/UiPepCtrlsNet/CheckedListBoxItemCollectionEditor.h
@@ -19,13 +19,20 @@ namespace Pep
 			CheckedListBoxItemCollectionEditor(System::Type^ type);
 
 		protected:
+			virtual System::Object^ CreateInstance(System::Type^ ItemType) override;
 			virtual System::ComponentModel::Design::CollectionEditor::CollectionForm^ CreateCollectionForm() override;
+
+		private:
+			void CollectionForm_FormClosed(System::Object^ sender, System::Windows::Forms::FormClosedEventArgs^ e);
 
 		protected:
 			/// <summary>
 			/// Clean up any resources being used.
 			/// </summary>
 			~CheckedListBoxItemCollectionEditor();
+
+		private:
+			System::ComponentModel::Design::CollectionEditor::CollectionForm^ m_CollectionForm;
 		};
 	};
 }
